0c1c6a1d30b0ea9dba6176fcb85a4b32c6e3c334
davidlygagnon <david.lygagnon@gmail.com>
2015-02-21 22:48:46 -0800

    adding library initial checkin

---
+++ fitbitClient.js
@@ -0,0 +1,54 @@
+var OAuth = require('oauth').OAuth;
+
+fitBitClient = (function () {
+	var baseUrl = 'https://api.fitbit.com',
+		requestPath = '/oauth/request_token',
+		accessPath = '/oauth/access_token',
+		oauth,
+		session = {};
+
+	return {
+		init: function (consumerKey, consumerSecret, callbackUrl) {
+			oauth = new OAuth(
+				baseUrl + requestPath,
+				baseUrl + accessPath,
+				consumerKey,
+				consumerSecret,
+				'1.0',
+				callbackUrl,
+				'HMAC-SHA1'
+			);  
+		},
+
+		getRequestToken: function (callback) {
+			oauth.getOAuthRequestToken(callback);
+		},
+
+		getAccessToken: function (token, token_secret, verifier, callback) {
+			oauth.getOAuthAccessToken(token, token_secret, verifier, callback);
+		},
+
+		requestResource: function (path, method, accessToken, accessTokenSecret, userId, callback) {
+			var url = baseUrl + '/1/user/' + (userId || '-') + path;
+			oauth.getProtectedResource(url, method, accessToken, accessTokenSecret, callback);
+		},
+
+		setToken: function (accessToken) {
+			session.accessToken = accessToken;
+		},
+		
+		getToken: function () {
+			return session.accessToken;
+		},
+
+		setTokenSecret: function (accessTokenSecret) {
+			session.accessTokenSecret = accessTokenSecret;
+		},
+
+		getTokenSecret: function () {
+			return session.accessTokenSecret;
+		}
+	}
+})();
+
+module.exports = fitBitClient;
