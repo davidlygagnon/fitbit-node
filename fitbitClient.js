6abaaad4ef73a031faf141402af415b1c9e2a274
davidlygagnon <david.lygagnon@gmail.com>
2015-02-27 23:12:54 -0800

    replace tabs by spaces

--- fitbitClient.js
+++ fitbitClient.js
@@ -1,58 +1,58 @@
 var OAuth = require('oauth').OAuth;
 
 fitBitClient = (function () {
-	var baseUrl = 'https://api.fitbit.com',
-		requestPath = '/oauth/request_token',
-		accessPath = '/oauth/access_token',
-		oauth,
-		session = {};
+    var baseUrl = 'https://api.fitbit.com',
+        requestPath = '/oauth/request_token',
+        accessPath = '/oauth/access_token',
+        oauth,
+        session = {};
 
-	return {
-		init: function (consumerKey, consumerSecret, callbackUrl) {
-			oauth = new OAuth(
-				baseUrl + requestPath,
-				baseUrl + accessPath,
-				consumerKey,
-				consumerSecret,
-				'1.0',
-				callbackUrl,
-				'HMAC-SHA1'
-			);  
-		},
+    return {
+        init: function (consumerKey, consumerSecret, callbackUrl) {
+            oauth = new OAuth(
+                baseUrl + requestPath,
+                baseUrl + accessPath,
+                consumerKey,
+                consumerSecret,
+                '1.0',
+                callbackUrl,
+                'HMAC-SHA1'
+            );  
+        },
 
-		getRequestToken: function (callback) {
-			oauth.getOAuthRequestToken(callback);
-		},
+        getRequestToken: function (callback) {
+            oauth.getOAuthRequestToken(callback);
+        },
 
-		getAccessToken: function (token, token_secret, verifier, callback) {
-			oauth.getOAuthAccessToken(token, token_secret, verifier, callback);
-		},
+        getAccessToken: function (token, token_secret, verifier, callback) {
+            oauth.getOAuthAccessToken(token, token_secret, verifier, callback);
+        },
 
-		requestResource: function (path, method, accessToken, accessTokenSecret, userId, callback) {
-			var url = baseUrl + '/1/user/' + (userId || '-') + path;
-			oauth.getProtectedResource(url, method, accessToken, accessTokenSecret, callback);
-		},
+        requestResource: function (path, method, accessToken, accessTokenSecret, userId, callback) {
+            var url = baseUrl + '/1/user/' + (userId || '-') + path;
+            oauth.getProtectedResource(url, method, accessToken, accessTokenSecret, callback);
+        },
 
-		setToken: function (accessToken) {
-			session.accessToken = accessToken;
-		},
-		
-		getToken: function () {
-			return session.accessToken;
-		},
+        setToken: function (accessToken) {
+            session.accessToken = accessToken;
+        },
+        
+        getToken: function () {
+            return session.accessToken;
+        },
 
-		setTokenSecret: function (accessTokenSecret) {
-			session.accessTokenSecret = accessTokenSecret;
-		},
+        setTokenSecret: function (accessTokenSecret) {
+            session.accessTokenSecret = accessTokenSecret;
+        },
 
-		getTokenSecret: function () {
-			return session.accessTokenSecret;
-		},
+        getTokenSecret: function () {
+            return session.accessTokenSecret;
+        },
 
-		getAuthorizeURL: function(oAuthToken) {
-			return 'http://www.fitbit.com/oauth/authorize?oauth_token=' + oAuthToken;
-		}
-	}
+        getAuthorizeURL: function(oAuthToken) {
+            return 'http://www.fitbit.com/oauth/authorize?oauth_token=' + oAuthToken;
+        }
+    }
 })();
 
 module.exports = fitBitClient;
